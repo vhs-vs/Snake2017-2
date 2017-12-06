f90e40bf4b51cf1ec32059e825947fb5564423ef
DataJockey <github@blackserver.de>
2017-12-06 11:32:50 +0100

    Game-Klassen als public deklariert

--- Snake/Game/Food.cs
+++ Snake/Game/Food.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Snake.Game
 {
-    class Food
+    public class Food
     {
     }
 }
--- Snake/Game/Game.cs
+++ Snake/Game/Game.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Snake.Game
 {
-    class Game
+    public class Game
     {
     }
 }
